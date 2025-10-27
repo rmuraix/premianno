f608c79da58b999f795f7285c4487c6f17e9b050
Ryota Murai <35632215+rmuraix@users.noreply.github.com>
2025-10-27 17:40:42 +0900

    chore: init lib

---
+++ packages/lib/vite.config.ts
@@ -0,0 +1,29 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { runAction, uxp } from "vite-uxp-plugin";
+
+import { config } from "./uxp.config";
+
+const action = process.env.BOLT_ACTION;
+const mode = process.env.MODE;
+
+if (action) runAction(config, action);
+
+const shouldNotEmptyDir =
+  mode === "dev" && config.manifest.requiredPermissions?.enableAddon;
+
+export default defineConfig({
+  plugins: [uxp(config, mode), react()],
+  build: {
+    sourcemap: !!(mode && ["dev", "build"].includes(mode)),
+    minify: false,
+    emptyOutDir: !shouldNotEmptyDir,
+    rollupOptions: {
+      external: ["premierepro", "uxp", "fs", "os", "path", "process", "shell"],
+      output: {
+        format: "cjs",
+      },
+    },
+  },
+  publicDir: "public",
+});
